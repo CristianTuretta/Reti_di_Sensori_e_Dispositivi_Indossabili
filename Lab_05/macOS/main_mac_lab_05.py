831f0cebf1224bb39dc659c52099ed5994e9b270
CristianTuretta <cristian.turetta@me.com>
2024-05-20 14:21:02 +0200

    Added comments to file

--- Lab_05/macOS/main_mac_lab_05.py
+++ Lab_05/macOS/main_mac_lab_05.py
@@ -1,13 +1,29 @@
-from utility.UUIDS import *
+import datetime
+import warnings
 from bleak import *
 
+# Ignore the FutureWarning kind of warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 async def scan():
+    """
+    Scan for BLE devices and print the results to the console
+    :return:
+    """
+    # Scan and save the list of devices
     devices = await BleakScanner.discover()
+
+    # Get the current time
+    scan_time = datetime.datetime.now()
+    print(f"Scan: {scan_time}")
+
+    # Print the list of devices found and some of their details
     for device in devices:
+        # Comment this line if you want to see all the BLE devices around
         if device.name == 'Thingy':
-            print(device.address)
+            print(f"    Device {device.address} | RSSI={device.rssi} dB")
 
 
 if __name__ == '__main__':
+    # Run the scan function
     asyncio.run(scan())
